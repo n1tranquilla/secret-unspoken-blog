fe6b325396d2ffa3411d56a82656519d6b8f8de8
Nathan Tranquilla <n1tranquilla@gmail.com>
2018-10-12 07:07:16 -0300

    Updates

--- sw.js
+++ sw.js
@@ -26,21 +26,21 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c6c30cb9fefbc5218ead.js"
+    "url": "webpack-runtime-e00b2fe02491d97e1420.js"
   },
   {
-    "url": "app-0f6b8e5b2da124172fa3.js"
+    "url": "app-970eac9dcc56124e79e8.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-c36ab03cf1f59fe9fd67.js"
   },
   {
     "url": "index.html",
-    "revision": "f003d4b151e2442fb6cbd408df2f891b"
+    "revision": "a51f85c65ee5768c2269b786a1d4c99c"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2855786bbd0e9e89ae3a351b9770d539"
+    "revision": "a2a2cb80307eff4d9f11bce7335e22bb"
   },
   {
     "url": "component---src-pages-index-js.233bbdcaf413050cf331.css"
@@ -49,7 +49,7 @@ self.__precacheManifest = [
     "url": "component---src-pages-index-js-e3d5d2f9021ffe45903a.js"
   },
   {
-    "url": "0-94b91552ae5aa181a304.js"
+    "url": "0-4a08181a8a49aed85920.js"
   },
   {
     "url": "static/d/248/path---index-6a9-GTSjmx3WgAGzj9trGr48EBMuM.json",
@@ -74,7 +74,7 @@ self.__precacheManifest = [
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/offline-plugin-app-shell-fallback/index.html", {
+workbox.routing.registerNavigationRoute("/secret-unspoken-blog/offline-plugin-app-shell-fallback/index.html", {
   whitelist: [/^[^?]*([^.?]{5}|\.html)(\?.*)?$/],
   blacklist: [/\?(.+&)?no-cache=1$/],
 });
