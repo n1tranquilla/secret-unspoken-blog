61b13cb00fa5c5801aaaaad16bd1dc0edce9445b
Nathan Tranquilla <n1tranquilla@gmail.com>
2018-10-12 06:47:45 -0300

    Some minor tweaks

--- src/components/BlogListItem.js
+++ src/components/BlogListItem.js
@@ -5,9 +5,11 @@ import styles from './BlogListItem.module.css'
 
 const BlogListItem = props => (
     <div className={styles.blogListItem}>
-        <span className={styles.date}>{props.date}</span>
-        <Link className={styles.title} to={props.to}><h2 className={styles.title}>{props.title}</h2></Link>
-        <span className={styles.excerpt}>{props.excerpt}</span>
+        <Link className={styles.link} to={props.to}>
+            <span className={styles.date}>{props.date}</span>
+            <h2 className={styles.title}>{props.title}</h2>
+            <span className={styles.excerpt}>{props.excerpt}</span>
+        </Link>
     </div>
 )
 
