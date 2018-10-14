ce12748fa555e02159072240e305ef9826479350
n1tranquilla <n1tranquilla@gmail.com>
2018-10-14 09:26:01 -0300

    Social engagement (#6)
    * add commenting with disqus
    
    * 1.1.0

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,16 +3,24 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Column from '../components/Column'
 import DateOfPost from '../components/DateOfPost'
+import Disqus from 'disqus-react'
 import "github-markdown-css"
 
-export default ({ data }) => {
+export default ({ data, location }) => {
+  const siteMeta = data.site.siteMetadata
   const post = data.markdownRemark
+  const disqusConfig = {
+    url: location.href,
+    identifier: post.fields.slug, 
+    title: post.frontmatter.title
+  }
   return (
     <Layout>
         <Column>
             <DateOfPost>{post.frontmatter.date}</DateOfPost>
             <h2>{post.frontmatter.title}</h2>
             <div className="markdown-body" dangerouslySetInnerHTML={{ __html: post.html }} />
+            <Disqus.DiscussionEmbed shortname={siteMeta.shortname} config={disqusConfig}/>
         </Column>
     </Layout>
   )
@@ -20,11 +28,19 @@ export default ({ data }) => {
 
 export const query = graphql`
   query($slug: String!) {
+    site {
+      siteMetadata {
+        shortname
+      }
+    }
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
         title
         date(formatString: "DD MMMM, YYYY")
+      }
+      fields {
+        slug
       }
     }
   }
