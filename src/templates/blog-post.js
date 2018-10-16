bfe4b4833639348229ad094c69d2cd246791695b
n1tranquilla <n1tranquilla@gmail.com>
2018-10-16 19:52:40 -0300

    add shareable link

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import Layout from "../components/Layout"
 import Column from '../components/Column'
 import DateOfPost from '../components/DateOfPost'
 import Disqus from 'disqus-react'
+import StoryEngagement from "../components/StoryEngagement"
 
 export default ({ data, location }) => {
   const siteMeta = data.site.siteMetadata
@@ -18,6 +19,7 @@ export default ({ data, location }) => {
         <Column>
             <DateOfPost>{post.frontmatter.date}</DateOfPost>
             <h2>{post.frontmatter.title}</h2>
+            <StoryEngagement url={location.href}/>
             <div className="markdown-body" dangerouslySetInnerHTML={{ __html: post.html }} />
             <Disqus.DiscussionEmbed shortname={siteMeta.shortname} config={disqusConfig}/>
         </Column>
