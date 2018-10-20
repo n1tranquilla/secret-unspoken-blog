c3a188211136ab4e2d1665323ac12d687e4e0d3b
n1tranquilla <n1tranquilla@gmail.com>
2018-10-20 08:42:12 -0300

    opt-out option for Google Analytics (#16)
    * opt-out option for ga
    
    * working properly with build
    
    * keyed on optout

---
+++ src/components/GoogleAnalyticsDisclaimer.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import styles from './GoogleAnalyticsDisclaimer.module.css'
+
+class GoogleAnalyticsDisclaimer extends React.Component {
+
+    state={
+        optOut: false
+    }
+
+    key = `ga-disable-${this.props.gaTrackingId}`
+
+    initWindowVar = () => window[this.key]=localStorage.getItem(this.key)!==undefined
+        ? localStorage.getItem(this.key)==="true" ? true : false
+        : false
+
+    componentDidMount(){ 
+        const optOut = this.initWindowVar()
+        this.setState({optOut})
+    }
+
+    toggle = (e) => {
+        e.preventDefault()
+        const stringBool = localStorage.getItem(this.key)
+        localStorage.setItem(this.key,stringBool==="true" ? "false" : "true")
+        
+        const optOut = this.initWindowVar()
+        this.setState({optOut})
+    }
+
+    render(){
+        return (
+            <div key={this.state.optOut} className={styles.disclaimer}>
+                <pre>This site uses Google Analytics. </pre>
+                <a href="/#" onClick={this.toggle}>
+                    { this.state.optOut ? 'Opt in.' : 'Opt out.' }
+                </a>
+            </div>
+        )
+    }
+}
+
+export default GoogleAnalyticsDisclaimer
