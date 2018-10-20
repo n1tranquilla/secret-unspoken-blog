3ca51ed2c016927de200492e15726d9ca9c4c765
n1tranquilla <n1tranquilla@gmail.com>
2018-10-20 08:40:14 -0300

    working properly with build

--- src/components/GoogleAnalyticsDisclaimer.js
+++ src/components/GoogleAnalyticsDisclaimer.js
@@ -5,34 +5,35 @@ import styles from './GoogleAnalyticsDisclaimer.module.css'
 class GoogleAnalyticsDisclaimer extends React.Component {
 
     state={
-        redrawKey:0
+        optOut: false
     }
 
     key = `ga-disable-${this.props.gaTrackingId}`
 
-    componentDidMount(){
-        window[this.key]=localStorage.getItem(this.key)!==undefined
-            ? localStorage.getItem(this.key)
-            : false
-    }
+    initWindowVar = () => window[this.key]=localStorage.getItem(this.key)!==undefined
+        ? localStorage.getItem(this.key)==="true" ? true : false
+        : false
 
-    redraw = () => this.setState({ redrawKey: this.state.redrawKey+1})
+    componentDidMount(){ 
+        const optOut = this.initWindowVar()
+        this.setState({optOut})
+    }
 
     toggle = (e) => {
         e.preventDefault()
         const stringBool = localStorage.getItem(this.key)
         localStorage.setItem(this.key,stringBool==="true" ? "false" : "true")
-        this.redraw()
-
-        window[this.key]=localStorage.getItem(this.key)==="true" ? true : false
+        
+        const optOut = this.initWindowVar()
+        this.setState({optOut})
     }
 
     render(){
         return (
             <div key={this.state.redrawKey} className={styles.disclaimer}>
                 <pre>This site uses Google Analytics. </pre>
-                <a href="#" onClick={this.toggle}>
-                    { localStorage.getItem(this.key)==="true" ? 'Opt in.' : 'Opt out.' }
+                <a href="/#" onClick={this.toggle}>
+                    { this.state.optOut ? 'Opt in.' : 'Opt out.' }
                 </a>
             </div>
         )
