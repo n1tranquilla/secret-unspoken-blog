29b298988c724824736d5ba3e970456e864b4174
n1tranquilla <n1tranquilla@gmail.com>
2018-10-28 13:39:56 -0300

    Delay rendering of subscribe components in subscribe page

--- src/pages/subscribe.js
+++ src/pages/subscribe.js
@@ -2,11 +2,14 @@ import React from 'react'
 import Subscribe from '../components/Subscribe'
 import Layout from '../components/Layout'
 import Column from '../components/Column'
+import Delay from '../components/Delay'
 
 const SubscribePage = props => (
     <Layout>
         <Column>
-            <Subscribe/>
+            <Delay>
+                <Subscribe/>
+            </Delay>
         </Column>
     </Layout>
 )
