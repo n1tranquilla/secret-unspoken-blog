922523916354d77b1ef44343eb7826ffbd44cb12
n1tranquilla <n1tranquilla@gmail.com>
2018-11-15 21:18:53 -0400

    a2hs hotfix

--- src/components/AddToHomeScreen.js
+++ src/components/AddToHomeScreen.js
@@ -2,24 +2,23 @@ import React from 'react'
 import MySnackbar from './MySnackbar'
 import { Link } from 'gatsby'
 
+
 class AddToHomeScreen extends React.Component {
 
     state={
         deferredPrompt: null,
-        notify: false
+        notify: false,
     }
 
-    handleAdd = () => {
+    handleAdd = (e) => {
+        e.preventDefault()
+
         const { deferredPrompt } = this.state
         deferredPrompt.prompt()
 
         deferredPrompt.userChoice
             .then((choiceResult) => {
-                // if (choiceResult.outcome === 'accepted') {
-                //     console.log('User accepted the A2HS prompt');
-                // } else {
-                //     console.log('User dismissed the A2HS prompt');
-                // }
+                localStorage.setItem('a2hs-chosen','true')
                 this.setState({ 
                     deferredPrompt: null ,
                     notify: false
@@ -42,6 +41,9 @@ class AddToHomeScreen extends React.Component {
     }
 
     render(){
+
+        if(localStorage.getItem('a2hs-chosen')==='true') return null
+
         return this.state.notify
             ? (<MySnackbar duration={Infinity} msg={<Link onClick={this.handleAdd}>Add Secret Unspoken to homescreen</Link>} />)
             : null
